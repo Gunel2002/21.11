2581478203da95b5efb2acbc966c5fbabcac5634
Gunel2002 <118666791+Gunel2002@users.noreply.github.com>
2022-11-28 21:37:35 +0900

    Add files via upload

---
+++ DLL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = MyLibrary.MyLibrary.Add(3, 5);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
